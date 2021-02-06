eef554856511dbe15496e36e4b85a60da9f8d3a4
Mike1336 <muzhikovm@bk.ru>
2021-02-06 19:30:14 +0300

    Create form template

--- src/app/social-contract/basic-information/basic-information.module.ts
+++ src/app/social-contract/basic-information/basic-information.module.ts
@@ -3,15 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+
+import { MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
 
 import { FormComponent } from './form/form.component';
-import { ModalComponent } from './modal/modal.component';
 
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'LL',
+  },
+  display: {
+    dateInput: 'DD.MM.YYYY',
+    monthYearLabel: 'YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'YYYY',
+  },
+};
 
 @NgModule({
   declarations: [
     FormComponent,
-    ModalComponent,
   ],
   imports: [
     // Angular
@@ -19,9 +33,21 @@ import { ModalComponent } from './modal/modal.component';
     // Angular Material
     MatInputModule,
     MatFormFieldModule,
+    MatSelectModule,
+    MatDatepickerModule,
   ],
   exports: [
     FormComponent,
+  ],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+    { provide: MAT_DATE_LOCALE, useValue: 'ru-RU' },
   ],
 })
 export class BasicInformationModule { }
--- src/app/social-contract/basic-information/form/form.component.ts
+++ src/app/social-contract/basic-information/form/form.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  public toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+
   constructor() { }
 
   public ngOnInit(): void {
