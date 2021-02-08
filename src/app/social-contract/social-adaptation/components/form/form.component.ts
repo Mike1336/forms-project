358a360ec574beb05835dbb330dab999fa4710ca
Mike1336 <muzhikovm@bk.ru>
2021-02-08 19:45:22 +0300

    Create layout for social-adaptation form

--- src/app/social-contract/social-adaptation/components/form/form.component.ts
+++ src/app/social-contract/social-adaptation/components/form/form.component.ts
@@ -1,11 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 
+const ELEMENT_DATA = [
+  {
+    name: 'Обучение',
+    plannedExecutionDate: '10.09.2020',
+    money: false,
+    socialPartner: 'Служба занятости',
+    controlOpinion: 'Выполнено',
+  },
+  {
+    name: 'Открытие ИП',
+    plannedExecutionDate: '12.09.2020',
+    money: false,
+    socialPartner: 'Служба занятости',
+    controlOpinion: 'Выполнено',
+  },
+  {
+    name: 'Покупка оборудования',
+    plannedExecutionDate: '15.09.2020',
+    money: false,
+    socialPartner: 'Министерство сельского хозяйства',
+    controlOpinion: 'Выполнено',
+  },
+  {
+    name: 'Покупка сырья',
+    plannedExecutionDate: '20.09.2020',
+    money: false,
+    socialPartner: 'Министерство сельского хозяйства',
+    controlOpinion: 'Выполнено',
+  },
+];
+
 @Component({
   selector: 'social-adaptation-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
+
+  public displayedColumns: string[] = ['name', 'plannedExecutionDate', 'money', 'socialPartner', 'controlOpinion', 'menu'];
+  public dataSource = ELEMENT_DATA;
 
   constructor() { }
 
--- src/app/social-contract/social-adaptation/social-adaptation.module.ts
+++ src/app/social-contract/social-adaptation/social-adaptation.module.ts
@@ -3,9 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { FormComponent } from './components/form/form.component';
 import { ModalComponent } from './components/modal/modal.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 
 @NgModule({
@@ -19,6 +24,11 @@ import { ModalComponent } from './components/modal/modal.component';
     // Angular Material
     MatInputModule,
     MatFormFieldModule,
+    MatTableModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule,
   ],
   exports: [
     FormComponent,
