cde5555aedb67f7d609c0f4695d4a36d3046b336
Mike1336 <muzhikovm@bk.ru>
2021-02-08 22:27:37 +0300

    Create layout for execution-result form

--- src/app/social-contract/execution-result/components/form/form.component.ts
+++ src/app/social-contract/execution-result/components/form/form.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+const ELEMENT_DATA = [
+  {
+    date: '10.09.2020',
+    familyMembersQuantity: '4',
+    totalFamilyIncome: '40 000',
+    averageFamilyIncome: '10 000',
+    concluded: 'Выполнено',
+  },
+];
+
 @Component({
   selector: 'execution-result-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
+
+  public displayedColumns: string[] = ['date', 'familyMembersQuantity', 'totalFamilyIncome', 'averageFamilyIncome', 'concluded', 'menu'];
+  public dataSource = ELEMENT_DATA;
 
   constructor() { }
 
--- src/app/social-contract/execution-result/execution-result.module.ts
+++ src/app/social-contract/execution-result/execution-result.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 import { FormComponent } from './components/form/form.component';
 import { ModalComponent } from './components/modal/modal.component';
@@ -19,6 +23,10 @@ import { ModalComponent } from './components/modal/modal.component';
     // Angular Material
     MatInputModule,
     MatFormFieldModule,
+    MatTableModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
   ],
   exports: [
     FormComponent,
